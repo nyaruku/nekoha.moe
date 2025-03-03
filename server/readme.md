b510745856a2a01bb1abdab28c7166a398218a12
“nyaruku” <38096703+nyaruku@users.noreply.github.com>
2025-03-03 22:07:58 +0000

    Convert server folder into a submodule

--- server/readme.md
+++
@@ -1,3 +0,0 @@
-# Backend Server for my Website
-Deployement Script: run.sh | restarts on error
-
--- server/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while true; do
-    echo "Starting Server...."
-    node server.cjs
-    echo "Server crashed. Restarting..."
-    sleep 1
-done
