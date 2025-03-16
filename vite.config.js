d3d213bc32c8d5d35aba63496683a640857365ac
“nyaruku” <38096703+nyaruku@users.noreply.github.com>
2025-03-16 22:27:03 +0000

    changes

--- vite.config.js
+++ vite.config.js
@@ -23,10 +23,12 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:5000', // Proxy API requests to Express backend
       '/api2': 'http://localhost:5001', // Proxy API requests to Express backend
+      /*
       '/api/live/osu': {
         target: 'http://localhost:5000/live', // Ensure the target URL matches the WebSocket namespace
         ws: true,  // Enable WebSocket proxy
       },
+      */
     },
   }
 })
